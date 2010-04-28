35bf406abe804935fe735c1187dfb6e33eeae571
Mike Dirolf <mike@10gen.com>
2010-04-28 06:50:56 -0700

    add some more slides

--- slides.js
+++ slides.js
@@ -4,17 +4,31 @@ db.dropDatabase();
 
 /*
  * What is the shell?
+ *  * Full JS shell
+ *  * MongoDB client
  * Uses
  *  * Admin scripting
  *  * Exploring/debugging
  *  * Learning
- *
  */
 
 db.deck.save({slide: 0,
               title: "CRUD and the JavaScript Shell",
               presenter: "Mike Dirolf, 10gen",
               handle: "@mdirolf"});
+db.deck.save({slide: 1,
+              question: "What is the shell?",
+              answer: "A better Powerpoint?"});
+db.deck.save({slide: 2,
+              question: "What is the shell?",
+              answer: "A full JavaScript environment"});
+db.deck.save({slide: 3,
+              question: "What is the shell?",
+              answer: "A reference MongoDB client"});
+db.deck.save({slide: 4,
+              "use cases": ["Administrative scripting",
+                            "Exploring and debugging",
+                            "Learning"]});
 
  // current slide
 var current = 0;
@@ -25,7 +39,6 @@ var next = function() {
     if (slide) {
         current++;
         delete slide._id;
-        delete slide.slide;
         printjson(slide);
     } else {
         print("The End!");
